96565cd1534b24761182581e73c7abf1d95e80b7
kbbryar <kbbryar@localhost.localdomain>
2021-01-23 15:27:17 -0600

    Added the url to the setup script

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='simpleclass',
     version="0.1.0",
+    url="https://github.com/kbbryar/python-test.git",
+
     packages=find_packages()
 )

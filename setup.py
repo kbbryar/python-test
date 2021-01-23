bfd52208a60afbc46aeaef25803e604e29e564bf
kbbryar <kbbryar@localhost.localdomain>
2021-01-23 15:43:09 -0600

    Updated the structure by moving the script into the script folder.
    Also updated the setup.py to reference a package_dir

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="Ken Bryar",
     description="This is a simple class to test the setup script.",
     url="https://github.com/kbbryar/python-test.git",
-    packages=find_packages(),
+    packages=['scripts/simpleclass.py'],
+    package_dir = {"simpleclass": "scripts"}
     license='LICENSE.txt'
 )

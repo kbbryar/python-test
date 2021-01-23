266ae172ccadfa1b2ce8da688ba89b6323a7125a
kbbryar <kbbryar@localhost.localdomain>
2021-01-23 15:36:16 -0600

    Added a reference to a license file

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version="0.1.0",
     description="This is a simple class to test the setup script.",
     url="https://github.com/kbbryar/python-test.git",
-    packages=find_packages()
+    packages=find_packages(),
+    license='LICENSE.txt'
 )

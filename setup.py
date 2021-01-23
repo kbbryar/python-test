63ee6518e9dc9db8690eebd2565e78728756942e
kbbryar <kbbryar@localhost.localdomain>
2021-01-23 15:33:39 -0600

    fixed a missing comma

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='simpleclass',
     version="0.1.0",
-    description="This is a simple class to test the setup script."
+    description="This is a simple class to test the setup script.",
     url="https://github.com/kbbryar/python-test.git",
     packages=find_packages()
 )

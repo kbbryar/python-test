759d3205be8f2d4fcd032ab18c07ac1ed253618a
kbbryar <kbbryar@localhost.localdomain>
2021-01-24 07:59:51 -0600

    Added a test to allow pytest to be executed on the installers end

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(
     description="This is a simple class to test the setup script.",
     url="https://github.com/kbbryar/python-test.git",
     packages=find_packages(),
+    tests_require=["pytest"],
 )
